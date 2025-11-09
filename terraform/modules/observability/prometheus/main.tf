63f552861655e871f3cefa80dec1abe049dcc540
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-11-08 21:39:53 -0700

    fix: align kube-prometheus-stack chart version to 65.1.0

--- terraform/modules/observability/prometheus/main.tf
+++ terraform/modules/observability/prometheus/main.tf
@@ -34,7 +34,7 @@ resource "helm_release" "prometheus_stack" {
   name       = "kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  version    = "61.4.0"
+  version    =  var.chart_version
 
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
