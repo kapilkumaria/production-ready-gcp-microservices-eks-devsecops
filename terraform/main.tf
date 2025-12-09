2317bdc18e5c2e6d3ab5b139c091e921423922f7
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-12-09 09:19:16 -0700

    fix name of irsa module in terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,13 +75,7 @@ module "irsa" {
   oidc_provider_arn       = module.eks.oidc_provider_arn
   account_id              = data.aws_caller_identity.current.account_id
 
-  # REQUIRED PARAMETERS
-  role_name        = "prod-ready-eks-irsa-role"
-  namespace        = "default"
-  service_account  = "ecr-access"
-
-  # Allow microservices to pull images from ECR
-  attach_ecr_policy = true
+  depends_on = [module.eks]
 }
 
 #############################################
