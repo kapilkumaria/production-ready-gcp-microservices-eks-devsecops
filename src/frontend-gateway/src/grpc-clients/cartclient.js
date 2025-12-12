48cf1b2f890363a74e54fcba019f540b0d64a38a
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-12-11 21:38:42 -0700

    Fix proto loading path in frontend-gateway

--- src/frontend-gateway/src/grpc-clients/cartclient.js
+++ src/frontend-gateway/src/grpc-clients/cartclient.js
@@ -1,37 +1,13 @@
-// import grpc from "@grpc/grpc-js";
-// import protoLoader from "@grpc/proto-loader";
-// import path from "path";
-
-// const PROTO_PATH = path.resolve("protos/cart/cart.proto");
-
-// const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-//   includeDirs: [path.resolve("protos")],
-//   keepCase: true,
-//   longs: String,
-//   enums: String,
-//   defaults: true,
-//   oneofs: true,
-// });
-
-// const proto = grpc.loadPackageDefinition(packageDefinition).hipstershop;
-
-// export const cartClient = new proto.CartService(
-//   "cartservice:7070",
-//   grpc.credentials.createInsecure()
-// );
-
-import grpc from "@grpc/grpc-js";
 import loadProto from "./loadProto.js";
-import dotenv from "dotenv";
-dotenv.config();
+import grpc from "@grpc/grpc-js";
 
-const proto = loadProto("cart_service.proto");
+const proto = loadProto("cart/Cart.proto");
 
-const CART_ADDR =
+// Because Cart.proto has: package hipstershop;
+const CartService = proto.hipstershop.CartService;
+
+export const cartClient = new CartService(
   process.env.CART_SERVICE_ADDR ??
-  "cartservice.cartservice.svc.cluster.local:7070";
-
-export const cartClient = new proto.CartService(
-  CART_ADDR,
+    "cartservice.cartservice.svc.cluster.local:7070",
   grpc.credentials.createInsecure()
 );
