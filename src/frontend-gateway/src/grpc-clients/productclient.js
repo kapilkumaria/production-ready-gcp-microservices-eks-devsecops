684c56f3e4457c3b126f64c95a0ffefa0e59bf99
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-12-12 10:13:34 -0700

    Fix frontend-gateway Dockerfile to include source and protos

--- src/frontend-gateway/src/grpc-clients/productclient.js
+++ src/frontend-gateway/src/grpc-clients/productclient.js
@@ -1,14 +1,10 @@
 import loadProto from "./loadProto.js";
 import grpc from "@grpc/grpc-js";
 
-// Loads: src/protos/productcatalog/product.proto
 const proto = loadProto("productcatalog/product.proto");
-
-// Package is: productcatalog
 const ProductCatalogService = proto.productcatalog.ProductCatalogService;
 
 export const productClient = new ProductCatalogService(
-  process.env.PRODUCTCATALOG_SERVICE_ADDR ??
-    "productcatalogservice.productcatalogservice.svc.cluster.local:3550",
+  process.env.PRODUCTCATALOG_SERVICE_ADDR,
   grpc.credentials.createInsecure()
 );
