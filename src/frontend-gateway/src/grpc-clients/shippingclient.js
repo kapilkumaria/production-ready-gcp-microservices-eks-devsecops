b16127c8b84e8db8f5380fd1f16f9ad1f96173d0
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-12-12 09:58:32 -0700

    Add ShippingService proto + fix shipping gRPC client

--- src/frontend-gateway/src/grpc-clients/shippingclient.js
+++ src/frontend-gateway/src/grpc-clients/shippingclient.js
@@ -1,38 +1,13 @@
-// import grpc from "@grpc/grpc-js";
-// import protoLoader from "@grpc/proto-loader";
-// import path from "path";
-
-// const PROTO_PATH = path.resolve("protos/shipping/shipping.proto");
-
-// const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-//   includeDirs: [path.resolve("protos")],
-//   keepCase: true,
-//   longs: String,
-//   enums: String,
-//   defaults: true,
-//   oneofs: true,
-// });
-
-// const proto = grpc.loadPackageDefinition(packageDefinition).hipstershop;
-
-// export const shippingClient = new proto.ShippingService(
-//   "shippingservice:50051",
-//   grpc.credentials.createInsecure()
-// );
-
 import grpc from "@grpc/grpc-js";
 import loadProto from "./loadProto.js";
-import dotenv from "dotenv";
-dotenv.config();
 
-const proto = loadProto("shipping_service.proto");
+const proto = loadProto("shipping/shipping_service.proto");
 
-const SHIPPING_ADDR =
+// package hipstershop
+const ShippingService = proto.hipstershop.ShippingService;
+
+export const shippingClient = new ShippingService(
   process.env.SHIPPING_SERVICE_ADDR ??
-  "shippingservice.shippingservice.svc.cluster.local:50051";
-
-export const shippingClient = new proto.ShippingService(
-  SHIPPING_ADDR,
+    "shippingservice.shippingservice.svc.cluster.local:50051",
   grpc.credentials.createInsecure()
 );
-
