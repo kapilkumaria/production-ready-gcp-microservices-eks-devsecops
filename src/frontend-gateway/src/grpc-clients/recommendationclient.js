f2c5d0998095115819d0e9685a7360e37d07f321
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-12-11 21:10:09 -0700

    Fix recommendation client proto loading

--- src/frontend-gateway/src/grpc-clients/recommendationclient.js
+++ src/frontend-gateway/src/grpc-clients/recommendationclient.js
@@ -1,38 +1,16 @@
-// import grpc from "@grpc/grpc-js";
-// import protoLoader from "@grpc/proto-loader";
-// import path from "path";
-
-// const PROTO_PATH = path.resolve("protos/recommendation/recommendation.proto");
-
-// const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-//   includeDirs: [path.resolve("protos")],
-//   keepCase: true,
-//   longs: String,
-//   enums: String,
-//   defaults: true,
-//   oneofs: true,
-// });
-
-// const proto = grpc.loadPackageDefinition(packageDefinition).hipstershop;
-
-// export const recommendationClient = new proto.RecommendationService(
-//   "recommendationservice:8085",
-//   grpc.credentials.createInsecure()
-// );
-
-
 import grpc from "@grpc/grpc-js";
 import loadProto from "./loadProto.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const proto = loadProto("recommendation_service.proto");
+// Recommendation service actually uses ProductCatalog proto
+const proto = loadProto("productcatalog/product_catalog.proto");
 
 const RECOMMEND_ADDR =
   process.env.RECOMMENDATION_SERVICE_ADDR ??
   "recommendationservice.recommendationservice.svc.cluster.local:8080";
 
-export const recommendationClient = new proto.RecommendationService(
+export const recommendationClient = new proto.productcatalog.RecommendationService(
   RECOMMEND_ADDR,
   grpc.credentials.createInsecure()
 );
