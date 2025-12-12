bb13d773eaf8281d811dece9f452cac3a856fd5d
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-12-12 09:51:00 -0700

    Fix frontend-gateway gRPC integration by adding missing protos and updating client loaders

--- src/frontend-gateway/src/grpc-clients/checkoutclient.js
+++ src/frontend-gateway/src/grpc-clients/checkoutclient.js
@@ -1,37 +1,13 @@
-// import grpc from "@grpc/grpc-js";
-// import protoLoader from "@grpc/proto-loader";
-// import path from "path";
-
-// const PROTO_PATH = path.resolve("protos/checkout/checkout.proto");
-
-// const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-//   includeDirs: [path.resolve("protos")],
-//   keepCase: true,
-//   longs: String,
-//   enums: String,
-//   defaults: true,
-//   oneofs: true,
-// });
-
-// const proto = grpc.loadPackageDefinition(packageDefinition).hipstershop;
-
-// export const checkoutClient = new proto.CheckoutService(
-//   "checkoutservice:5050",
-//   grpc.credentials.createInsecure()
-// );
-
 import grpc from "@grpc/grpc-js";
 import loadProto from "./loadProto.js";
-import dotenv from "dotenv";
-dotenv.config();
 
-const proto = loadProto("checkout_service.proto");
+const proto = loadProto("checkout/checkout_service.proto");
 
-const CHECKOUT_ADDR =
+// Because package is hipstershop in the proto
+const CheckoutService = proto.hipstershop.CheckoutService;
+
+export const checkoutClient = new CheckoutService(
   process.env.CHECKOUT_SERVICE_ADDR ??
-  "checkoutservice.checkoutservice.svc.cluster.local:5050";
-
-export const checkoutClient = new proto.CheckoutService(
-  CHECKOUT_ADDR,
+    "checkoutservice.checkoutservice.svc.cluster.local:5050",
   grpc.credentials.createInsecure()
 );
